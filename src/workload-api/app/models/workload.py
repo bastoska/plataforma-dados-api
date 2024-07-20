a05ebd37c52c37fda78691e0c75b7dc0ba88b813
Nash Bastos <nashbastos@gmail.com>
2024-07-20 22:02:41 +0000

    feat: add models

---
+++ src/workload-api/app/models/workload.py
@@ -0,0 +1,27 @@
+from tinydb import TinyDB, Query
+from tinydb.storages import MemoryStorage
+from datetime import datetime
+
+db = TinyDB('app/data/db.json')  # In-memory TinyDB for testing
+
+class Workload:
+    def __init__(self):
+        self.table = db.table('workloads')
+
+    def insert(self, workload):
+        self.table.insert(workload)
+
+    def get(self, workload_id: int):
+        query = Query()
+        return self.table.get(query.id == workload_id)
+
+    def update(self, workload_id: int, update_data):
+        query = Query()
+        self.table.update(update_data, query.id == workload_id)
+
+    def delete(self, workload_id: int):
+        query = Query()
+        self.table.remove(query.id == workload_id)
+
+    def get_all(self):
+        return self.table.all()
