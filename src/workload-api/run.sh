64a8a359a073b2ec8a48047e88480c60a0312f55
Nash Bastos <nashbastos@gmail.com>
2024-07-22 20:45:00 +0000

    chore: add pytest

--- src/workload-api/run.sh
+++ src/workload-api/run.sh
@@ -1,17 +1,50 @@
 
 
 up_container () {
+
+  ## variáveis para conter o nome do container e da imagem
   container_name=workload_api
   image_name=$container_name:1.0
 
+  ## removendo o container (caso não exista, ele seguirá para o próximo comando)
   docker rm -f $container_name
 
+  ## construção da imagem
   docker image build -t $image_name .
 
+  ## execução do container em modo deamon
   docker run -d -p 8080:8080 -v ./volume/data:/app/app/data --name $container_name $image_name
 
+  ## visualizar continuamente os logs do container
   docker logs -f $container_name
 }
 
+up_container_test () {
+  ## variáveis para conter o nome do container e da imagem
+  container_name=workload_api-test
+  image_name=$container_name:1.0
 
-up_container
+  ## removendo o container (caso não exista, ele seguirá para o próximo comando)
+  docker rm -f $container_name
+
+  ## construção da imagem
+  docker image build -f DockerfileTest -t $image_name .
+
+  ## execução do container em modo deamon
+  docker run -d --name $container_name $image_name
+  
+  ## visualizar continuamente os logs do container
+  docker logs -f $container_name
+}
+
+case $1 in
+  up)
+    up_container
+    ;;
+  test)
+    up_container_test
+    ;;
+  *)
+    echo "Usage: $0 {up|test|down}"
+    ;;
+esac
