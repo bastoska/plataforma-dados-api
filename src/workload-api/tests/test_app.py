7852bf54421f9e73b416a35eb13d5754a1c62ea8
Nash Bastos <nashbastos@gmail.com>
2024-07-23 15:13:05 +0000

    test: add project/dataset/table existence

--- src/workload-api/tests/test_app.py
+++ src/workload-api/tests/test_app.py
@@ -1,12 +1,12 @@
 import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from fastapi import FastAPI
 from app.app import app
 from app.models.workload import db
 
 @pytest.fixture
-def anyio_backend():
-    return 'asyncio'
+def client():
+    return TestClient(app)
 
 # @pytest.fixture(autouse=True)
 # def setup_and_teardown_db():
@@ -14,15 +14,18 @@ def anyio_backend():
 #     db.drop_tables()
 #     db.create_tables()
 
-@pytest.mark.anyio
-async def test_read_root():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/")
+def test_read_root(client):
+    """
+    Test the root route
+    """
+    response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Running.."}
 
-@pytest.mark.anyio
-async def test_create_workload():
+def test_create_workload(client):
+    """
+    Teste the creation of a correct workload
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -31,8 +34,7 @@ async def test_create_workload():
         "src_table": "table1",
         "src_column_name": "column1"
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/api/v1/workloads/", json=workload)
+    response = client.post("/api/v1/workloads/", json=workload)
     assert response.status_code == 200
     data = response.json()
     assert data["workload_type"] == workload["workload_type"]
@@ -43,8 +45,10 @@ async def test_create_workload():
     assert data["src_column_name"] == workload["src_column_name"]
     assert data["status"] == "created"
 
-@pytest.mark.anyio
-async def test_create_workload_wrong_type():
+def test_create_workload_wrong_type(client):
+    """
+    Test the creation of a workload with a wrong field data type
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -53,13 +57,13 @@ async def test_create_workload_wrong_type():
         "src_table": "table1",
         "src_column_name": 123  # should be a string
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/api/v1/workloads/", json=workload)
+    response = client.post("/api/v1/workloads/", json=workload)
     assert response.status_code == 422
 
-
-@pytest.mark.anyio
-async def test_create_workload_missing_field():
+def test_create_workload_missing_field(client):
+    """
+    Test the creation of a workload with missing required field
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -67,12 +71,13 @@ async def test_create_workload_missing_field():
         "src_dataset": "dataset1",
         "src_table": "table1",
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/api/v1/workloads/", json=workload)
+    response = client.post("/api/v1/workloads/", json=workload)
     assert response.status_code == 422
 
-@pytest.mark.anyio
-async def test_read_workload():
+def test_read_workload(client):
+    """
+    Test the read of a workload with a given ID
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -81,12 +86,13 @@ async def test_read_workload():
         "src_table": "table1",
         "src_column_name": "column1"
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/api/v1/workloads/", json=workload)
+    ## creating a workload to get an id to request
+    response = client.post("/api/v1/workloads/", json=workload)
     assert response.status_code == 200
     workload_id = response.json()["id"]
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get(f"/api/v1/workloads/{workload_id}")
+
+    ## getting the created workload with the given workload_id
+    response = client.get(f"/api/v1/workloads/{workload_id}")
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == workload_id
@@ -97,8 +103,10 @@ async def test_read_workload():
     assert data["src_table"] == workload["src_table"]
     assert data["src_column_name"] == workload["src_column_name"]
 
-@pytest.mark.anyio
-async def test_read_all_workloads():
+def test_read_all_workloads(client):
+    """
+    Test the read all workloads
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -107,15 +115,16 @@ async def test_read_all_workloads():
         "src_table": "table1",
         "src_column_name": "column1"
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        await ac.post("/api/v1/workloads/", json=workload)
-        response = await ac.get("/api/v1/workloads/")
+    client.post("/api/v1/workloads/", json=workload)
+    response = client.get("/api/v1/workloads/")
     assert response.status_code == 200
     data = response.json()
     assert type(data) == list
 
-@pytest.mark.anyio
-async def test_delete_workload():
+def test_delete_workload(client):
+    """
+    Test the deletion of the given workload id
+    """
     workload = {
         "workload_type": "type1",
         "name": "Test Workload",
@@ -124,12 +133,13 @@ async def test_delete_workload():
         "src_table": "table1",
         "src_column_name": "column1"
     }
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/api/v1/workloads/", json=workload)
+    ## we first create a workload to have an id to delete
+    response = client.post("/api/v1/workloads/", json=workload)
     assert response.status_code == 200
     workload_id = response.json()["id"]
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.delete(f"/api/v1/workloads/{workload_id}")
+
+    ## deleting the given workload_id
+    response = client.delete(f"/api/v1/workloads/{workload_id}")
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == workload_id
@@ -139,3 +149,52 @@ async def test_delete_workload():
     assert data["src_dataset"] == workload["src_dataset"]
     assert data["src_table"] == workload["src_table"]
     assert data["src_column_name"] == workload["src_column_name"]
+
+
+def test_create_nonexist_prj(client):
+    """
+    Test the deletion of a workload with a nonexistent google cloud project
+    """
+    workload = {
+        "workload_type": "type1",
+        "name": "Test Fail Project",
+        "src_project": "telconet_missing_project",
+        "src_dataset": "dataset1",
+        "src_table": "table1",
+        "src_column_name": "column1"
+    }
+    response = client.post("/api/v1/workloads/", json=workload)
+    assert response.status_code == 404
+
+
+def test_create_nonexist_dataset(client):
+    """
+    Test the deletion of a workload with a nonexistent BQ dataset
+    """
+    workload = {
+        "workload_type": "type1",
+        "name": "Test Fail Dataset",
+        "src_project": "project1",
+        "src_dataset": "telconet_missing_dataset",
+        "src_table": "table1",
+        "src_column_name": "column1"
+    }
+    response = client.post("/api/v1/workloads/", json=workload)
+    assert response.status_code == 404
+
+
+def test_create_nonexist_table(client):
+    """
+    Test the deletion of a workload with a nonexistent BQ table
+    """
+    workload = {
+        "workload_type": "type1",
+        "name": "Test Fail Table",
+        "src_project": "project1",
+        "src_dataset": "dataset1",
+        "src_table": "telconet_missing_table",
+        "src_column_name": "column1"
+    }
+    response = client.post("/api/v1/workloads/", json=workload)
+    assert response.status_code == 404
+    
